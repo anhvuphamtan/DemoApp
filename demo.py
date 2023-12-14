8c9b08436e10fda9c1b3472e2034e293c25b94de
AnhVuPhamTan <anhvuphamtan@gmail.com>
2023-12-14 15:04:19 +0700

    Init demo app

---
+++ demo.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+import pandas as pd
+import dotenv
+import faker
+import boto3
+import os
+
+def setVarGlobal():
+    dotenv.load_dotenv()
+    global AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, REGION, BUCKET_NAME, BUCKET_KEY, HOST_NAME
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    REGION = os.getenv('REGION')
+    BUCKET_NAME = os.getenv('BUCKET_NAME')
+    BUCKET_KEY  = os.getenv('BUCKET_KEY')
+    HOST_NAME   = os.getenv('HOST_NAME')
+
+def readDataFromS3():
+    S3_client = boto3.client(
+        "s3",
+        aws_access_key_id = AWS_ACCESS_KEY_ID,
+        aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
+        region_name = REGION 
+    )
+
+    response = S3_client.get_object(
+        Bucket = BUCKET_NAME,
+        Key    = BUCKET_KEY
+    )
+
+    data = response['Body']
+
+    return data
+
+def processingData(data):
+    cnt = 0
+    for line in data: 
+        cnt += 1
+    
+    with open(f"{HOST_NAME}.log", "w") as file:
+        file.write(f"{HOST_NAME} - {datetime.now()} - Found {cnt} records in {BUCKET_KEY}")
+    
+def main():
+    data = readDataFromS3()
+    processingData(data)
+
+if __name__ == "__main__":
+    setVarGlobal()
+    main()
